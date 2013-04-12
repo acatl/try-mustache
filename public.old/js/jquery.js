dfbf174e981c4ac06cde355c1e3535fcaf8f948b
acatl <acatl.pacheco@gmail.com>
2013-04-12 11:20:57 -0500

    copy changes, trying to improve SEO :S

--- public.old/js/jquery.js
+++
@@ -1,4 +0,0 @@
-/**
-@codekit-prepend "plugins/jquery-1.9.0.js"
-@codekit-prepend "plugins/jquery-ui-1.10.0.min.js"
-**/
--- public.old/js/plugins.js
+++
@@ -1,6 +0,0 @@
-/**
-@codekit-prepend "plugins/codemirror.js"
-@codekit-prepend "plugins/cm/modes/javascript/javascript.js"
-@codekit-prepend "plugins/cm/modes/xml/xml.js"
-@codekit-prepend "plugins/cm/modes/htmlmixed/htmlmixed.js"
-**/
